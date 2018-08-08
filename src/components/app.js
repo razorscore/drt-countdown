fe4cf75fd18f6b4e4580f47732b0dd08f834b7f5
Daven Triplett <daventriplett@gmail.com>
2018-08-08 08:44:03 -0600

    app setup

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>DRT Countdown</h1>
+      </div>
+    );
+  }
+}

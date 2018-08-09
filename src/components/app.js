274960975ba45063bfcb109573890bacb6ae9bf0
Daven Triplett <daventriplett@gmail.com>
2018-08-09 08:07:58 -0600

    added title

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div className="grid">
+        <h1 className="grid__title">DRT Countdown</h1>
+      
         <div className="grid__skew-dark-one"></div>
         <div className="grid__skew-dark-two"></div>
         <div className="grid__skew-dark-three"></div>
